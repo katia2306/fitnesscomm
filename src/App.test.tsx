691e1e0f8b5be2dbab89b58c2ce744a95ff640a5
RodyGL <rody.guzman@outlook.com>
2019-03-28 01:00:08 -0700

    App test setup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,37 @@
+import "./bootstrapMUI";
+/* Remove previous lines after Material-UI v4 release */
+
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { Route, Router } from "react-router-dom";
+import NoSsr from "@material-ui/core/NoSsr";
 import App from "./App";
+import browserHistory from "./browserHistory";
+import "./index.css";
+import store from "./store/redux.store";
 
-// eslint-disable-next-line no-undef
+window.matchMedia = jest.fn().mockImplementation(query => {
+  return {
+    matches: false,
+    media: query,
+    onchange: undefined,
+    addListener: jest.fn(),
+    removeListener: jest.fn()
+  };
+});
+
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <NoSsr>
+      <Router history={browserHistory}>
+        <Provider store={store}>
+          <Route component={App} />
+        </Provider>
+      </Router>
+    </NoSsr>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
