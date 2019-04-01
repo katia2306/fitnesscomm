a4ece8358e46b30788d15f37865e0df8a04da748
RodyGL <rody.guzman@outlook.com>
2019-03-31 23:23:44 -0700

    Migrate cloud functions

--- server/lib/firebase.admin.js
+++
@@ -1,4 +0,0 @@
-"use strict";var _interopRequireWildcard = require("@babel/runtime/helpers/interopRequireWildcard");Object.defineProperty(exports, "__esModule", { value: true });exports.db = exports.admin = void 0;var firebaseAdmin = _interopRequireWildcard(require("firebase-admin"));
-
-const admin = firebaseAdmin.initializeApp();exports.admin = admin;
-const db = admin.firestore();exports.db = db;
