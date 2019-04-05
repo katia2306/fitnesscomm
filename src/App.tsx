8269726f7013ef9d3c6728ec12d802863334fd26
RodyGL <rody.guzman@outlook.com>
2019-04-04 21:49:16 -0700

    Workaround for notistack children proptype

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React, { Fragment } from "react";
 import { connect } from "react-redux";
-import { Route, Switch } from "react-router-dom";
-import { ThemeProvider } from "@material-ui/styles";
+import { ThemeProvider, makeStyles } from "@material-ui/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { SnackbarProvider } from "notistack";
-import { Button } from "@material-ui/core";
+import { Button, Grid } from "@material-ui/core";
+import { Route, Switch } from "react-router-dom";
 import { AppNavigation } from "./components";
-import { Home, Profile } from "./pages";
 import ReduxModel from "./store/redux.model";
 import { themeSelectors } from "./store/theme.reducer";
 import { darkTheme, lightTheme } from "./themes";
 import { userSelectors } from "./store/user.reducer";
-import UserRoute from "./routes/UserRoute";
 import { setAPIUrl } from "./utils/api.utils";
+import UserRoute from "./routes/UserRoute";
+import { Home, Profile } from "./pages";
 
 setAPIUrl();
 
@@ -21,25 +21,41 @@ interface Props {
   isThemeDark: ReduxModel["theme"]["isThemeDark"];
 }
 
-const AppContent = (
-  <Fragment>
-    <CssBaseline />
-    <AppNavigation />
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <UserRoute exact path="/profile" component={Profile} />
-      <Route component={undefined} />
-    </Switch>
-  </Fragment>
-);
+const useStyles = makeStyles({
+  root: {
+    padding: "16px"
+  }
+});
 
 const App = (props: Props) => {
   const { isThemeDark, userLoaded } = props;
+  const classes = useStyles();
+
   const theme = isThemeDark ? darkTheme : lightTheme;
 
   if (!userLoaded) {
     return <div>Loading...</div>;
   }
+
+  const AppContent = (
+    <Fragment>
+      <CssBaseline />
+      <AppNavigation />
+      <Grid
+        container
+        justify="center"
+        className={classes.root}
+        component="main"
+        spacing={8}
+      >
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <UserRoute exact path="/profile" component={Profile} />
+          <Route component={undefined} />
+        </Switch>
+      </Grid>
+    </Fragment>
+  );
 
   return (
     <ThemeProvider theme={theme}>
