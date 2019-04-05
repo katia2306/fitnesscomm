9db3bebdf0c815e9d07cf8e1312c598c98c8519c
RodyGL <rody.guzman@outlook.com>
2019-04-04 17:27:09 -0700

    Remove restricted imports

--- src/components/ThemeToggle/ToggleTheme.tsx
+++ src/components/ThemeToggle/ToggleTheme.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Brightness4, Brightness4Outlined } from "@material-ui/icons";
-// eslint-disable-next-line no-restricted-imports
 import IconButton, { IconButtonProps } from "@material-ui/core/IconButton";
 import ReduxModel from "../../store/redux.model";
 import { themeSelectors, themeActions } from "../../store/theme.reducer";
