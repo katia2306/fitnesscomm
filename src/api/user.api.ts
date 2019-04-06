096405e5e3f8fe8e5b801488b7ae24eec9b64aaf
RodyGL <rody.guzman@outlook.com>
2019-04-05 19:23:39 -0700

    Edit users api

--- src/api/user.api.ts
+++ src/api/user.api.ts
@@ -2,9 +2,9 @@ import axios from "axios";
 import { User } from "../store/user.reducer";
 
 const userAPI = {
-  getUser: () => axios.get("users/current_user").then(res => res.data.user),
+  getUser: () => axios.get("users-api/current_user").then(res => res.data.user),
   signupUser: (user: Partial<User>) =>
-    axios.post("users/signup", user).then(res => res.data)
+    axios.post("users-api/signup", user).then(res => res.data)
 };
 
 export default userAPI;
